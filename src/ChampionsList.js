e0081dc30cc77d166d2cfb035322fdc33e4e6da0
FColombati <fcolombati@adimpex.it>
2025-09-04 17:13:26 +0200

    Aggiunta Akeron

--- src/ChampionsList.js
+++ src/ChampionsList.js
@@ -1,258 +1,264 @@
 export const defaultChampionsFile = `[
     {
         "id": 1,
+        "name": "Akeron",
+        "image": "images/Champions/Akeron.png",
+        "roles": ["jungle", "offlane"]
+    },
+    {
+        "id": 2,
         "name": "Argus",
         "image": "images/Champions/Argus.png",
         "roles": ["midlane", "support"]
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "Aurora",
         "image": "images/Champions/Aurora.png",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 3,
+        "id": 4,
         "name": "Boris",
         "image": "images/Champions/Boris.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 4,
+        "id": 5,
         "name": "Countess",
         "image": "images/Champions/Countess.png",
         "roles": ["midlane", "jungle"]
     },
     {
-        "id": 5,
+        "id": 6,
         "name": "Crunch",
         "image": "images/Champions/Crunch.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 6,
+        "id": 7,
         "name": "Dekker",
         "image": "images/Champions/Dekker.png",
         "roles": ["support"]
     },
     {
-        "id": 7,
+        "id": 8,
         "name": "Drongo",
         "image": "images/Champions/Drongo.png",
         "roles": ["adc"]
     },
     {
-        "id": 8,
+        "id": 9,
         "name": "Feng Mao",
         "image": "images/Champions/FengMao.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 9,
+        "id": 10,
         "name": "Gadget",
         "image": "images/Champions/Gadget.png",
         "roles": ["midlane"]
     },
     {
-        "id": 10,
+        "id": 11,
         "name": "Gideon",
         "image": "images/Champions/Gideon.png",
         "roles": ["midlane"]
     },
     {
-        "id": 11,
+        "id": 12,
         "name": "GRIM.exe",
         "image": "images/Champions/GRIM.exe.png",
         "roles": ["adc", "midlane"]
     },
     {
-        "id": 12,
+        "id": 13,
         "name": "Greystone",
         "image": "images/Champions/Greystone.png",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 13,
+        "id": 14,
         "name": "Grux",
         "image": "images/Champions/Grux.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 14,
+        "id": 15,
         "name": "Howitzer",
         "image": "images/Champions/Howitzer.png",
         "roles": ["midlane"]
     },
     {
-        "id": 15,
+        "id": 16,
         "name": "Iggy & Scorch",
         "image": "images/Champions/Iggy&Scorch.png",
         "roles": ["midlane"]
     },
     {
-        "id": 16,
+        "id": 17,
         "name": "Kallari",
         "image": "images/Champions/Kallari.png",
         "roles": ["jungle"]
     },
     {
-        "id": 17,
+        "id": 18,
         "name": "Khaimera",
         "image": "images/Champions/Khaimera.png",
         "roles": ["jungle"]
     },
     {
-        "id": 18,
+        "id": 19,
         "name": "Kira",
         "image": "images/Champions/Kira.png",
         "roles": ["adc"]
     },
     {
-        "id": 19,
+        "id": 20,
         "name": "Kwang",
         "image": "images/Champions/Kwang.jpg",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 20,
+        "id": 21,
         "name": "Lt. Belica",
         "image": "images/Champions/LtBelica.png",
         "roles": ["midlane", "support"]
     },
     {
-        "id": 21,
+        "id": 22,
         "name": "Morigesh",
         "image": "images/Champions/Morigesh.png",
         "roles": ["midlane"]
     },
     {
-        "id": 22,
+        "id": 23,
         "name": "Mourn",
         "image": "images/Champions/Mourn.png",
         "roles": ["support"]
     },
     {
-        "id": 23,
+        "id": 24,
         "name": "Murdock",
         "image": "images/Champions/Murdock.png",
         "roles": ["adc"]
     },
     {
-        "id": 24,
+        "id": 25,
         "name": "Muriel",
         "image": "images/Champions/Muriel.png",
         "roles": ["support"]
     },
     {
-        "id": 25,
+        "id": 26,
         "name": "Narbash",
         "image": "images/Champions/Narbash.png",
         "roles": ["support"]
     },
     {
-        "id": 26,
+        "id": 27,
         "name": "Phase",
         "image": "images/Champions/Phase.png",
         "roles": ["support"]
     },
     {
-        "id": 27,
+        "id": 28,
         "name": "Rampage",
         "image": "images/Champions/Rampage.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 28,
+        "id": 29,
         "name": "Revenant",
         "image": "images/Champions/Revenant.png",
         "roles": ["adc"]
     },
     {
-        "id": 29,
+        "id": 30,
         "name": "Riktor",
         "image": "images/Champions/Riktor.png",
         "roles": ["support", "offlane"]
     },
     {
-        "id": 30,
+        "id": 31,
         "name": "Serath",
         "image": "images/Champions/Serath.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 31,
+        "id": 32,
         "name": "Sevarog",
         "image": "images/Champions/Sevarog.png",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 32,
+        "id": 33,
         "name": "Shinbi",
         "image": "images/Champions/Shinbi.png",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 33,
+        "id": 34,
         "name": "Skylar",
         "image": "images/Champions/Skylar.png",
         "roles": ["adc"]
     },
     {
-        "id": 34,
+        "id": 35,
         "name": "Sparrow",
         "image": "images/Champions/Sparrow.png",
         "roles": ["adc"]
     },
     {
-        "id": 35,
+        "id": 36,
         "name": "Steel",
         "image": "images/Champions/Steel.png",
         "roles": ["support", "offlane"]
     },
     {
-        "id": 36,
+        "id": 37,
         "name": "Terra",
         "image": "images/Champions/Terra.jpg",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 37,
+        "id": 38,
         "name": "The Fey",
         "image": "images/Champions/TheFey.png",
         "roles": ["midlane"]
     },
     {
-        "id": 38,
+        "id": 39,
         "name": "TwinBlast",
         "image": "images/Champions/TwinBlast.png",
         "roles": ["adc"]
     },
     {
-        "id": 39,
+        "id": 40,
         "name": "Wraith",
         "image": "images/Champions/Wraith.png",
         "roles": ["adc", "midlane"]
     },
     {
-        "id": 40,
+        "id": 41,
         "name": "Yin",
         "image": "images/Champions/Yin.png",
         "roles": ["jungle"]
     },
     {
-        "id": 41,
+        "id": 42,
         "name": "Yurei",
         "image": "images/Champions/Yurei.png",
         "roles": ["jungle", "offlane"]
     },
     {
-        "id": 42,
+        "id": 43,
         "name": "Zarus",
         "image": "images/Champions/Zarus.png",
         "roles": ["offlane", "jungle"]
     },
     {
-        "id": 43,
+        "id": 44,
         "name": "Zinx",
         "image": "images/Champions/Zinx.jpg",
         "roles": ["support"]
